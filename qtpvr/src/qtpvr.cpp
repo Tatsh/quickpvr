2a02e72c0a973b2d16c3cc40d0f1451638172289
Andrew Udvare <audvare@gmail.com>
2018-06-28 14:28:13 -0400

    Figure out the format from the string; add todo

--- qtpvr/src/qtpvr.cpp
+++ qtpvr/src/qtpvr.cpp
@@ -4,6 +4,36 @@
 #include <QGraphicsScene>
 #include <pvr.h>
 
+static QImage::Format getFormatFromString(const char *inFormat) {
+    if (!inFormat) {
+        return QImage::Format_Invalid;
+    }
+
+    QString format(inFormat);
+    if (format.isNull()) {
+        return QImage::Format_Invalid;
+    }
+
+    if (format == "RGBA8888") {
+        return QImage::Format_RGBA8888;
+    }
+    if (format == "RGB555") {
+        return QImage::Format_RGB555;
+    }
+    if (format == "RGB888") {
+        return QImage::Format_RGB888;
+    }
+    if (format == "I8") {
+        return QImage::Format_Indexed8;
+    }
+    if (format == "AI8") {
+        return QImage::Format_Alpha8;
+    }
+
+    // RGBA4444, RGBA5551, RGB565, PVRTC2, PVRTC4
+    return QImage::Format_Invalid;
+}
+
 qtpvr::qtpvr(QWidget *parent, const char *imagePath) :
     QMainWindow(parent),
     ui(new Ui::qtpvr),
@@ -17,10 +47,19 @@ qtpvr::qtpvr(QWidget *parent, const char *imagePath) :
     }
 
     if (pvr.data) {
+        // TODO Add a toolbar with buttons to override these properties because
+        // sometimes files don't have the correct flags set
         qDebug("Format: %s", pvr.format);
         qDebug("Should flip: %s", pvr.should_flip ? "true" : "false");
 
-        QImage image(pvr.data, pvr.width, pvr.height, QImage::Format_RGBA8888);
+        QImage::Format format = getFormatFromString(pvr.format);
+
+        if (format == QImage::Format_Invalid) {
+            return;
+        }
+
+        QImage image(pvr.data, pvr.width, pvr.height, format);
+
         if (!image.isNull()) {
             QPixmap pixmap = QPixmap::fromImage(image);
             scene->addPixmap(pixmap);
